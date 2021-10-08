066fd9b7df7ab3184395b987d9837a4a104536be
Sarah Beaubeau <sarah.beaubeau@hotmail.fr>
2021-10-08 14:07:19 +0200

    Animation au click, début de l'insertion des images en js

--- script.js
+++ script.js
@@ -1,59 +1,22 @@
-//Code cours - en attente live 5 octobre
+//Animation au click
 
-/*const panels = document.querySelector('.container > *'); 
+const panels = document.querySelectorAll('.container > *'); 
 
-panels[0].addEventListener('click', function () {
+for (let panel of panels) {
+    panel.addEventListener('click', function() {
+        panels.forEach(p => p.classList.remove('active'));
+        panel.classList.add('active');
+    });
+}
 
-});*/
+//Insertion des images - En attente du cours du 12 octobre
+
+const panelDiv = document.createElement('div');
+panelDiv.style = "background-image: url(./img/normandy.png)";
+panelDiv.className = "panel active";
 
 
-//Mon essai perso = fonctionne mais sans doute inutilement long
 
-const panels = document.getElementsByClassName('panel');
-console.log(panels);
-
-panels[0].addEventListener('click', function () {
-    panels[0].classList.add("active");
-    panels[1].classList.remove("active");
-    panels[2].classList.remove("active");
-    panels[3].classList.remove("active");
-    panels[4].classList.remove("active");
-    console.log("Panel 0 cliqué");
-    console.log(panels);
-});
-panels[1].addEventListener('click', function () {
-    panels[1].classList.add("active");
-    panels[0].classList.remove("active");
-    panels[2].classList.remove("active");
-    panels[3].classList.remove("active");
-    panels[4].classList.remove("active");
-    console.log("Panel 1 cliqué");
-    console.log(panels);
-});
-panels[2].addEventListener('click', function () {
-    panels[2].classList.add("active");
-    panels[1].classList.remove("active");
-    panels[0].classList.remove("active");
-    panels[3].classList.remove("active");
-    panels[4].classList.remove("active");
-    console.log("Panel 2 cliqué");
-    console.log(panels);
-});
-panels[3].addEventListener('click', function () {
-    panels[3].classList.add("active");
-    panels[1].classList.remove("active");
-    panels[2].classList.remove("active");
-    panels[0].classList.remove("active");
-    panels[4].classList.remove("active");
-    console.log("Panel 3 cliqué");
-    console.log(panels);
-});
-panels[4].addEventListener('click', function () {
-    panels[4].classList.add("active");
-    panels[1].classList.remove("active");
-    panels[2].classList.remove("active");
-    panels[3].classList.remove("active");
-    panels[0].classList.remove("active");
-    console.log("Panel 4 cliqué");
-    console.log(panels);
-});
+//            <div style="background-image: url(./img/normandy.png)" class="panel">
+//                <h3>Normandy SR1</h3>
+//            </div>
