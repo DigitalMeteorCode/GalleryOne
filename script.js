9f88aec843af9e1a1d01d2eec38f448ef1476dba
Sarah Beaubeau <sarah.beaubeau@hotmail.fr>
2021-10-22 16:57:00 +0200

    Cours du 19 octobre - class et constructor

--- script.js
+++ script.js
@@ -1,44 +1,35 @@
 //Insertion des images et événement au click
 
+//Import de la class panel
+import { Panel } from "./script/panel.js";
 
 //Recherche des données images
-const images = [
-    "normandy",
-    "grass",
-    "illos",
-    "redmoon",
-    "selenee"
-];
+//const panelImages = [
+//  new Panel("normandy"),
+//  new Panel("grass"),
+//  new Panel("illos"),
+//  new Panel("redmoon"),
+//  new Panel("selenee")
+//];
+
 //Recherche des données titres -en attente
-const titles = [
-    "Normandy SR1",
-    "Planète inconnue",
-    "Illos",
-    "Lune rouge",
-    "Sélénée"
-]
+const panelTitles = [
+  new Panel("Normandy SR1"),
+  new Panel("Planète inconnue"),
+  new Panel("Illos"),
+  new Panel("Lune rouge"),
+  new Panel("Sélénée"),
+];
+
+for (let panelTitle of panelTitles) {
+  panelTitle.render();
+}
 
 //Logique d'application et écouteur d'événement click
-for (let image of images) {
-    const panel = createPanel(image);
-    panel.addEventListener('click', function() {
-        ///panels.forEach(p => p.classList.remove('active')); en attente
-        panel.classList.add('active');
-    });
-}
-
-//Fonction de création des panels
-function createPanel(img) {
-    const panelDiv = document.createElement('div');//créer un élément div
-    panelDiv.style = "background-image: url(img/" + img + ".png)";//lui dire d'avoir ce style -image de background-
-    panelDiv.className = "panel";//lui dire d'avoir cette classe
-    panelDiv.innerHTML = "<h3>titre</h3>";//lui dire de contenir un titre h3
-    document.querySelector('.container').appendChild(panelDiv);//lui dire de s'insérer dans l'élément avec la classe container
-    return panelDiv;
-}
-
-
-
-//<div style="background-image: url(./img/normandy.png)" class="panel">
-//    <h3>Normandy SR1</h3>
-//</div>
+//for (let image of images) {
+//  const panel = createPanel(image);
+//  panel.addEventListener("click", function () {
+//    ///panels.forEach(p => p.classList.remove('active')); en attente
+//    panel.classList.add("active");
+//  });
+//}
--- script/panel.js
+++ script/panel.js
@@ -1,3 +1,24 @@
-class Panel {
-    //en attente cours 19/10
+//pour pouvoir importer la class panel, il faut d'abord l'exporter, et préciser sur le fichier html que script.js est de type module
+export class Panel {
+  constructor(title = "No title") {
+    const div = document.createElement("div");
+    this.html = div;
+    this.initialize(div, title);
+    //créer un élément div
+  }
+
+  initialize(div, title) {
+    //div.style = "background-image: url(img/" + img + ".png)";
+    div.style = "background-image: url(img/normandy.png)";
+    //lui dire d'avoir ce style -image de background-
+    div.className = "panel";
+    //lui dire d'avoir cette classe
+    div.innerHTML = "<h3>" + title + "</h3>";
+    //lui dire de contenir un titre h3
+  }
+
+  render() {
+    document.querySelector(".container").appendChild(this.html);
+    //lui dire de s'insérerdans l'élément avec la classe container
+  }
 }
